0a66ac71a3ad4376b901b617043240da2a166e05
ruhee.dewji <ruhee.dewji@invivo.com>
2015-09-22 20:10:14 -0400

    Extreme data clean (still WIP), add special capitalization cases

--- js/sloanipsum.js
+++ js/sloanipsum.js
@@ -13,13 +13,6 @@ function generateIpsum(numberOfParagraphs) {
 
 	// Known broken stuff:
 	// looping strings split at apostrophe is failing (undefined or repeated strings)
-	// some lines with commas are breaking the line that follows (probably because of spaces)
-
-	// TODO:
-	// convert checks to regexes, lazybones
-	// Clean up uncapitalize for special cases (make array or something)
-	// Handle special uncapitalize cases for 'S E A N', 'P L A Y', 'Jolene'
-	// Check data for more split quotations
 
 	for(var p = 0; p < numberOfParagraphs; p++) {
 		// we want all the paragraphs to be different lengths
@@ -29,29 +22,30 @@ function generateIpsum(numberOfParagraphs) {
 			var ipsumLine = lyrics[Math.floor(Math.random()*lyrics.length)];
 
 			// uncapitalize line if chained together with commas
+			// and if also not one of the million special cases (G turns to D, S E A N S A I D, etc.)
 			if(ipsumText.trim().slice(-1) == ',') {
-				if(ipsumLine.slice(0,2) != 'I ' && ipsumLine.slice(0,2) != "I'" && ipsumLine.slice(0,6) != 'Johnny' && ipsumLine.slice(0,4) != 'Sean' && ipsumLine.slice(0,3) != 'Ana') {
-					ipsumLine = uncapitalize(ipsumLine);
+				if(ipsumLine.slice(0,2) != 'I ' && ipsumLine.slice(0,2) != "I'" && // lol
+					ipsumLine.slice(0,6) != 'Johnny' &&
+					ipsumLine.slice(0,4) != 'Sean' &&
+					ipsumLine.slice(0,3) != 'Ana' &&
+					ipsumLine.slice(0,6) != 'Jolene' &&
+					ipsumLine.slice(0,2) != 'S ' &&
+					ipsumLine.slice(0,2) != 'P ' &&
+					ipsumLine.slice(0,2) != 'G ' &&
+					ipsumLine.slice(0,9) != 'Halloween') {
+
+						ipsumLine = uncapitalize(ipsumLine);
 				}
 
-				// catch leading apostrophes and parentheticals, uncapitalize second char there instead
-				// YES this is redundant and ugly. TODO: fix once strings are all coming back ok
-				if(ipsumLine.slice(0,1) == '(' || ipsumLine.slice(0,1) == "'") {
-					if(ipsumLine.slice(0,1) == '(' && (ipsumLine.slice(1,3) != 'I ' && ipsumLine.slice(1,3) != "I'")) {
-						var newLine = ipsumLine.split('(');
-						newLine[1] = uncapitalize(newLine[1]);
-						ipsumLine = '(' + newLine[1];
-					}
-					else if(ipsumLine.slice(0,1) == "'") {
-						var newLine = ipsumLine.split("'");
-						newLine[1] = uncapitalize(newLine[1]);
-						console.log(newLine)
-						ipsumLine = "'";
+				// catch leading apostrophes, uncapitalize second char there instead
+				if(ipsumLine.slice(0,1) == "'") {
+					var newLine = ipsumLine.split("'");
+					newLine[1] = uncapitalize(newLine[1]);
+					console.log(newLine)
+					ipsumLine = "'";
 
-						for(var j = 1; j <= newLine.length; j++) {
-							ipsumLine += newLine[i]
-
-						}
+					for(var j = 1; j <= newLine.length; j++) {
+						ipsumLine += newLine[i]
 					}
 				}
 			}
@@ -59,10 +53,10 @@ function generateIpsum(numberOfParagraphs) {
 			// randomly decide whether to string into longer sentence
 			// I hate regex
 			var lastChar = ipsumLine.slice(-1);
-			var lastCharPunctuation = (lastChar != '.' && lastChar != '?' && lastChar != ',' && lastChar != '!');
+			var lastCharNotPunctuation = (lastChar != '.' && lastChar != '?' && lastChar != ',' && lastChar != '!');
 
 			if(randBoolean() && i < paragraphLength-1 && chainedLines < 4) {
-				if(lastCharPunctuation){
+				if(lastCharNotPunctuation){
 					// console.log('fulfilled if statements, comma added to (before):',ipsumLine)
 					ipsumLine += ', ';
 				}
@@ -73,7 +67,7 @@ function generateIpsum(numberOfParagraphs) {
 				chainedLines++;
 			}
 			else {
-				if(lastCharPunctuation){
+				if(lastCharNotPunctuation){
 					// console.log('if conditions, period added to:',ipsumLine);
 					ipsumLine += '. ';
 				}
@@ -90,6 +84,7 @@ function generateIpsum(numberOfParagraphs) {
 			ipsumText += '</p><p>';
 		}
 		else {
+			// check for punctuation here too, slice and add a period if a comma
 			ipsumText += '</p>';
 		}
 
